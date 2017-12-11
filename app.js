fa171824ad00e37dc6829d26a4bfbbc8fd3ca15c
wizehack <wizehack@gmail.com>
2017-12-11 20:21:37 +0900

    [feat] add result file upload API
    :Release:
    add result file upload API
    
    :Details:
    uploadResult POST API is added
    package.json file is updated
    
    :Test:
    Unit Test
    
    :Issues:
    N/A
    
    :Reference:
    N/A

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var mkdirp = require('mkdirp');
+var fs = require('fs');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
@@ -26,6 +28,7 @@ var port = process.env.PORT || 52270;
 // configure router
 var tcRouter = require('./routes/tcRouter')(app, TestCase);
 var resultRouter = require('./routes/resultRouter')(app, TestSuite);
+var uploadRouter = require('./routes/uploadRouter')(app, fs, mkdirp, __dirname);
 
 app.listen(52270, function () {
     var ip = '127.0.0.1';
---
+++ routes/uploadRouter.js
@@ -0,0 +1,43 @@
+module.exports = function(app, fs, mkdirp, homeDir) {
+    app.post('/uploadResult/:id/:file', function(req, res) {
+        var testsuiteId = req.params.id;
+        var filename = req.params.file;
+        var body = '';
+        var path = homeDir + '/data' + '/' + testsuiteId;
+
+        if (fs.existsSync(path) === false) {
+            console.log('new: ' + path);
+            mkdirp(path, function (err) {
+                if (err) {
+                    console.error(err)
+                }
+                else {
+                    console.log('created')
+                }
+            });
+        }
+
+        path = path + '/' + filename;
+
+        req.on('data', function(data) {
+            console.log('data: ' + data);
+            body += data;
+        });
+
+        req.on('end', function (){
+            console.log('end event');
+            if (fs.existsSync(path) === true) {
+                console.log('update: ' + path);
+                fs.unlinkSync(path);
+                fs.appendFile(path, body, function() {
+                    res.end();
+                });
+            } else {
+                console.log('create: ' + path);
+                fs.appendFile(path, body, function() {
+                    res.end();
+                });
+            }
+        });
+    });
+};
